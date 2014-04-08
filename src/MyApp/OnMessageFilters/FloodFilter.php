b07b126adb353f52731cdf577caf52a0139c86b5
Kubintsev A. N <kubintsev.ru@gmail.com>
2014-04-08 09:32:56 +0400

    flood filter fix

--- src/MyApp/OnMessageFilters/FloodFilter.php
+++ src/MyApp/OnMessageFilters/FloodFilter.php
@@ -5,6 +5,7 @@ namespace MyApp\OnMessageFilters;
 use MyApp\Chain\ChainContainer;
 use MyApp\Chain\ChainInterface;
 use MyApp\ChatConfig;
+use MyApp\Clients\User;
 use MyApp\Clients\UserCollection;
 use MyApp\MightyLoop;
 use MyApp\Response\ErrorResponse;
@@ -14,38 +15,74 @@ use MyApp\Utils\Lang;
 class FloodFilter implements ChainInterface
 {
 	use TSingleton;
-	private $userTimers;
+	private $otherTimers = [];
+	private $pingTimers = [];
 
 	public function handleRequest(ChainContainer $chain)
 	{
 		$user = $chain->getFrom();
-		$loop = MightyLoop::get()->fetch();
-
-		$prevRequest = isset($this->userTimers[$user->getId()]) ? $this->userTimers[$user->getId()] : false;
 
 		if (isset($chain->getRequest()['subject']) && $chain->getRequest()['subject'] == 'Ping') {
-			return;
+			return $this->manageRequest($user, $this->pingTimers);
 		}
 
-		if ($prevRequest) {
-			$response = (new ErrorResponse())
-				->setErrors(['flood' => Lang::get()->getPhrase('FloodDetected')])
-				->setChatId($user->getChatId());
-			(new UserCollection())
-				->setResponse($response)
-				->attach($user)
-				->notify(false);
+		return $this->manageRequest($user, $this->otherTimers);
+	}
 
+	/**
+	 * @param User $user
+	 * @param array $timers
+	 * @return bool
+	 */
+	private function manageRequest(User $user, array &$timers)
+	{
+		if ($this->isRequestHot($user, $timers)) {
+			$this->respondFloodError($user);
 			return false;
-
 		}
 
-		$timerCallback = function() use ($user) {
-			unset($this->userTimers[$user->getId()]);
+		$this->setTimer($user, $timers);
+	}
+
+	/**
+	 * @param User $user
+	 * @param array $timers
+	 * @return bool
+	 */
+	private function isRequestHot(User $user, array $timers)
+	{
+		return isset($timers[$user->getId()]) ? $timers[$user->getId()] : false;
+	}
+
+	/**
+	 * @param User $user
+	 */
+	private function respondFloodError(User $user)
+	{
+		$response = (new ErrorResponse())
+			->setChatId($user->getChatId())
+			->setErrors(['flood' => Lang::get()->getPhrase('FloodDetected')]);
+
+		(new UserCollection())
+			->setResponse($response)
+			->attach($user)
+			->notify(false);
+	}
+
+	/**
+	 * @param User $user
+	 * @param array $timers
+	 * @return array
+	 */
+	private function setTimer(User $user, array &$timers)
+	{
+		$loop = MightyLoop::get()->fetch();
+		$timerCallback = function () use ($user, &$timers) {
+			unset($timers[$user->getId()]);
 		};
 
 		$timer = $loop->addTimer(ChatConfig::get()->getConfig()->floodTimeout, $timerCallback);
 
-		$this->userTimers[$user->getId()] = $timer;
+		$timers[$user->getId()] = $timer;
 	}
 }
