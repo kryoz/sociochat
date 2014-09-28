c978ec7c4b04471cc5a09a953a1f46c791b1e77d
Kubintsev Alexander <alex@kubintsev.ru>
2014-09-28 21:12:31 +0400

    mp3 download

--- www/pages/audio/head.php
+++ www/pages/audio/head.php
@@ -4,6 +4,6 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-	<title>SocioChat - Музыка</title>
+	<title>СоциоЧат - Музыка</title>
 </head>
 <body>
--- www/pages/audio/listen.php
+++ www/pages/audio/listen.php
@@ -1,19 +1,8 @@
-<table>
-	<td><b><?=$dao->getArtist()?></b> - <?=$dao->getSong()?></td>
-	<td><?=$dao->getQuality()?> kbit/sec</td>
-</table>
-<br>
-<audio controls>
-	<source src="<?=$url?>" type='audio/mpeg; codecs="mp3"' >
-	Ваш браузер не поддерживает тег audio!
-</audio>
-<br>
-<input type="text" id="url-container" value="https://sociochat.me/audio.php?track_id=<?=$dao->getTrackId()?>"/>
-<button id="copy-button" data-clipboard-target="url-container" data-clipboard-text="Default clipboard text from attribute" data-copied-hint="Скопировано!" title="Скопировать ссылку в буфер обмена">Скопировать ссылку в буфер обмена</button>
-<br>
-<h3><a href="<?=$url?>">Скачать</a></h3>
-<p><small>СоциоЧат не хранит звуковые файлы. Источником данных служит сервис pleer.com</small></p>
-<script src="js/zeroclipboard.min.js"></script>
+<?php
+require_once "head.php";
+?>
 <script>
-	var client = new ZeroClipboard( document.getElementById("copy-button") );
+	window.location.href = '<?=$url?>';
 </script>
+</body>
+</html>
