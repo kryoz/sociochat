4db655f3cd1cdc8bba9aeeba5578719b3d4f0052
Kubintsev Alexander <kan@jackslip.com>
2014-10-03 19:05:46 +0400

    migration updat

--- db/migrations/20140922103107_fix_wrong_date_type.php
+++ db/migrations/20140922103107_fix_wrong_date_type.php
@@ -4,20 +4,15 @@ use Phinx\Migration\AbstractMigration;
 
 class FixWrongDateType extends AbstractMigration
 {
-    public function change()
-    {
-        $table = $this->table('user_properties');
-        $table
-            ->changeColumn('birth', 'date', ['default' => '1930-01-01'])
-            ->update();
-    }
-
     /**
      * Migrate Up.
      */
     public function up()
     {
-    
+	    $table = $this->table('user_properties');
+	    $table
+		    ->changeColumn('birth', 'date', ['default' => '1930-01-01'])
+		    ->update();
     }
 
     /**
@@ -25,6 +20,9 @@ class FixWrongDateType extends AbstractMigration
      */
     public function down()
     {
-
+	    $table = $this->table('user_properties');
+	    $table
+		    ->changeColumn('birth', 'timestamp', ['default' => null, 'null' => true])
+		    ->update();
     }
 }
