42e5fa1b369b54ca995c36d427b77d0883603128
Kubintsev Alex <kubintsev.ru@gmail.com>
2017-02-27 22:27:30 +0300

    users list on the right

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -43,6 +43,7 @@ define('app', function () {
 
         domElems: {
             guestList: $('#guests'),
+            guestMiniList: $('#guests-mini'),
             inputMessage: $('#message'),
             charsLeft : $('#charsLeft'),
             chat: $('#log'),
@@ -191,6 +192,8 @@ define('app', function () {
             $this.connection.onopen = function (e) {
                 if ($this.isFirstConnect) {
                     $this.domElems.chat.empty();
+                    $this.domElems.chat.parent().addClass('w80fl');
+                    $('.w20fl').removeClass('hidden');
                     $this.isFirstConnect = false;
                 }
 
@@ -233,11 +236,11 @@ define('app', function () {
 
                 $this.domElems.inputMessage.attr('disabled', 'disabled');
                 $this.domElems.inputMessage.attr('placeholder', 'Обрыв соединения... подождите, пожалуйста...');
-            }
+            };
 
             $this.connection.onerror = function (e) {
                 console.log(e);
-            }
+            };
 
             $this.connection.onmessage = function (e) {
                 try {
--- www/js/app/main.js
+++ www/js/app/main.js
@@ -2,5 +2,4 @@ define(['config'], function (config) {
     require(['app'], function (App) {
         App.Init(config.wsDomain, config.webDomain, config.protocol, config.maxMsgLength);
     });
-
 });
